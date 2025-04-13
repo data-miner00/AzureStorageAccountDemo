9fae69910efe60a4febc911fb75b64b83bc41b79
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-13 19:02:49 +0800

    Add project files.

---
+++ AzureStorageAccountDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+
+namespace AzureStorageAccountDemo
+{
+    internal class Program
+    {
+        internal const string StorageAccountName = "devstoreaccount1";
+        internal const string StorageAccountConnectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10003/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+
+        static async Task Main(string[] args)
+        {
+            var client = new BlobServiceClient(StorageAccountConnectionString);
+
+            var response = await client.CreateBlobContainerAsync("testcontainer");
+
+            var blobClient = response.Value;
+
+            await blobClient.UploadBlobAsync("test.txt", new BinaryData("Hello, World!"));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# AzureStorageAccountDemo
+
+Demo for Azure Storage Account using Azurite.
+
+## Prerequisites
+
+- Docker
+- Node.js
+- npm
+- Azurite
+- Azure Storage Explorer
+- Azure CLI
+
+## Todo
+
+Write in an article to explain the steps needed to work with blob storage, queues, and tables using Azurite.
+
+## Links
+
+- https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=visual-studio%2Cblob-storage
+- https://learn.microsoft.com/en-us/azure/azure-functions/functions-event-grid-blob-trigger?pivots=programming-language-csharp
+- https://github.com/Azure/Azurite
