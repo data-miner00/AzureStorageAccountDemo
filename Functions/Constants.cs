de8f16fa9cec51d0274f486d30fda35bc810aec7
Shaun Chong <data-miner00@users.noreply.github.com>
2025-04-16 21:28:13 +0800

    feat: Add functions

---
+++ Functions/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Constants
+    {
+        public const string StorageAccountName = "devstoreaccount1";
+
+        public const string TestContainerName = "testcontainer";
+    }
+}
